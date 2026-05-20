4291d1a32daa434d28604401bbda606e7d6e2fcc
havishjammula <havish.jammula47@gmail.com>
2026-05-20 13:38:06 -0400

    Added the overall top production players and top salaries

--- CalculateBangForBuck.py
+++ CalculateBangForBuck.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect(':memory:')
 df.to_sql('nfl_stats', conn, index=False)
 
 # SQL Query to calculate the players with the "Most Bang for Buck"
-sql_query = """
+sql_query_bang_for_buck = """
 SELECT 
     player_display_name, 
     position, 
@@ -22,7 +22,34 @@ WHERE position IN ('QB', 'RB', 'WR', 'TE')
 ORDER BY value_per_million DESC
 """
 
-analysis_df = pd.read_sql_query(sql_query, conn)
+analysis_df = pd.read_sql_query(sql_query_bang_for_buck, conn)
+
+sql_query = """
+SELECT 
+    player_display_name, 
+    position, 
+    production_score
+FROM nfl_stats
+WHERE position IN ('QB', 'RB', 'WR', 'TE')
+ORDER BY production_score DESC
+LIMIT 20
+"""
+
+overall_df = pd.read_sql_query(sql_query, conn)
+
+sql_query = """
+SELECT
+    player_display_name, 
+    position, 
+    Salary
+FROM nfl_stats
+WHERE position IN ('QB', 'RB', 'WR', 'TE')
+ORDER BY Salary DESC
+LIMIT 20
+"""
+
+salary_df = pd.read_sql_query(sql_query, conn)
+
 conn.close()
 
 # Scatter plot of Production Score vs. Salary,
@@ -43,7 +70,19 @@ plt.legend(title='Position')
 plt.grid(True, linestyle='--', alpha=0.6)
 plt.savefig('nfl_value_scatter.png')
 
+top_20_salries = salary_df.head(20)
+print("Top 20 Highest Paid Players")
+print(top_20_salries[['player_display_name', 'position', 'Salary']])
+
+print()
+
+top_20_overall = overall_df.head(20)
+print("Top 20 Most Productive Players (Overall)")
+print(top_20_overall[['player_display_name', 'position', 'production_score']])
+
+print()
+
 # Print the top 20 "Most Bang for your Buck" players
-top_20 = analysis_df.head(20)
+top_20_bang_for_buck = analysis_df.head(20)
 print("Top 20 'Most Bang for your Buck' Players")
-print(top_20[['player_display_name', 'position', 'value_per_million']])
+print(top_20_bang_for_buck[['player_display_name', 'position', 'value_per_million']])
