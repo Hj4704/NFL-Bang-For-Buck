b9de1dc0e342d62c7edd787e262bad9df161598b
havishjammula <havish.jammula47@gmail.com>
2026-07-17 11:36:08 -0400

    Finished up model to predict future production stats

--- FantasyPrediction.py
+++ FantasyPrediction.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 
 df_2025_stats = pd.read_csv('nfl_master_seasonal_stats_2025.csv')
-df_schedule = pd.read_csv('nfl_schedule_2026.csv')
-df_defensive_ratings = pd.read_csv('nfl_defensive_ratings.csv')
+df_schedule = pd.read_csv('schedule_2026.csv')
+df_defensive_ratings = pd.read_csv('defensive_ratings.csv')
 df_2025_stats = df_2025_stats[df_2025_stats['position'].isin(['QB', 'RB', 'WR', 'TE'])]
 
 def_lookup = dict(zip(df_defensive_ratings['team'], df_defensive_ratings['rank']))
@@ -16,12 +16,34 @@ for team in df_schedule['home_team'].unique():
     home_opponents = df_schedule[df_schedule['home_team'] == team]['away_team'].tolist()
     away_opponents = df_schedule[df_schedule['away_team'] == team]['home_team'].tolist()
     team_opponents[team] = home_opponents + away_opponents
-
-for index, row in df_2025_stats.iterrows():
+player_sos = []
+for idx, row in df_2025_stats.iterrows():
     player_team = row['team']
     opponents = team_opponents.get(player_team, [])
+    
+    if len(opponents) == 0:
+        player_sos.append(global_avg_rank)
+        continue
 
-ratings = [def_lookup.get(opp, global_avg_rank) for opp in opponents]
-player_strength_of_schedule.append(np.mean(ratings))
+    ratings = [def_lookup.get(opp, global_avg_rank) for opp in opponents]
+    player_sos.append(np.mean(ratings))
 
-df_2025_stats['2026_opponent_def_rating'] = player_strength_of_schedule
+df_2025_stats['2026_opponent_def_rating'] = player_sos
+
+X = df_2025_stats[['production_score', '2026_opponent_def_rating']]
+
+Y = df_2025_stats['production_score'] * (df_2025_stats['2026_opponent_def_rating'] / global_avg_rank)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=42)
+
+model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+model.fit(X_train, y_train)
+
+
+
+df_2025_stats['predictions_2026'] = model.predict(X).round(2)
+
+output_df = df_2025_stats[['player_display_name', 'position', 'team', 'production_score', '2026_opponent_def_rating', 'predictions_2026']]
+output_df.to_csv('nfl_projections_2026.csv', index=False)
+
+print(output_df.sort_values(by='predictions_2026', ascending=False).head(25))
